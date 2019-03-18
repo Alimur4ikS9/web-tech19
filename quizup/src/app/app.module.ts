f501637f33e89d4991fee137b7c61fecdae5d471
Alimur4ikS9 <alinur.sabit.9@gmail.com>
2019-03-18 15:27:11 +0600

    Created components

--- quizup/src/app/app.module.ts
+++ quizup/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { PartnersComponent } from './partners/partners.component';
+import { TestComponent } from './test/test.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    PartnersComponent,
+    TestComponent,
+    ProfileComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
